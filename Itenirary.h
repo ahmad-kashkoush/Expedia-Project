7832a034e693fba1c4e4c05f4a9b3e4fb68718e7
ahmed-kashkoush <ahmedkashkoush464@gmail.com>
2023-06-22 14:04:47 +0300

    Finished Polymorphism with apis and reservations

--- Itenirary.h
+++ Itenirary.h
@@ -4,28 +4,13 @@
 
 #ifndef EXPEDIA_PROJECT_ITENIRARY_H
 #include "Menu.h"
-#include "expedia_flights_api.h"
-#include "expedia_hotels_api.h"
+
 #define EXPEDIA_PROJECT_ITENIRARY_H
 class IteneraryItem{
 public:
     virtual IteneraryItem *Clone()=0;
     virtual double GetCost()=0;
     virtual const string &ToString()=0;
-};
-class IHotel: public IteneraryItem{
-    double TotalNights;
-    double PricePerNight;
-public:
-    IHotel(double a, double b):
-    TotalNights(a), PricePerNight(b){}
-    double GetCost(){
-        return TotalNights*PricePerNight;
-    }
-    void AddInfo(){
-
-    }
-
 };
 class Itenerary{
 private:
@@ -34,7 +19,17 @@ public:
     void AddIteneraryItem(IteneraryItem *item){
         Items.push_back(item->Clone());
     }
-
+    double TotalCost(){
+        double sum=0;
+        for(auto item:Items){
+            sum+=item->GetCost();
+        }
+    }
+    void ListItenerary(){
+        for(auto item:Items){
+            cout<<"\t"<<item->ToString()<<"\n";
+        }
+    }
 };
 
 
