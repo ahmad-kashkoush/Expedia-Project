42cb467e002fe364f493809207e6ae906bdd1cc4
ahmed-kashkoush <ahmedkashkoush464@gmail.com>
2023-06-25 03:33:43 +0300

    copy Paste Loaddatabase from the project

--- User.h
+++ User.h
@@ -3,7 +3,7 @@
 //
 
 #ifndef EXPEDIA_PROJECT_USER_H
-#include "Menu.h"
+#include "Utilities.h"
 #define EXPEDIA_PROJECT_USER_H
 class User{
 private:
@@ -57,6 +57,44 @@ public:
         users.push_back(User("conan", "223"));
 
     }
+    void LoadDatabase() {
+        cout << "UsersManager: LoadDatabase\n";
+//
+//        FreeLoadedData();
+//
+//        // Some "Dummy Data" for simplicity
+//        Admin* admin_user = new Admin();
+//        admin_user->SetUserName("mostafa");
+//        admin_user->SetPassword("111");
+//        admin_user->SetEmail("mostafa@gmail.com");
+//        //admin_user->SetIsLibraryAdmin(false);
+//        admin_user->SetName("mostafa Saad Ibrahim");
+//        userame_userobject_map[admin_user->GetUserName()] = admin_user;
+//
+//        Customer* customrUser = new Customer();
+//        customrUser->SetUserName("asmaa");
+//        customrUser->SetPassword("222");
+//        customrUser->SetEmail("asmaa@gmail.com");
+//        //user1->SetIsLibraryAdmin(true);
+//        customrUser->SetName("Asmaa Saad Ibrahim");
+//
+//        DebitCard* debitCard = new DebitCard();
+//        debitCard->SetOwnerName(customrUser->GetName());
+//        debitCard->SetCardNumber("11-22-33-44");
+//        debitCard->SetExpiryDate("11/20");
+//        debitCard->SetSecurityCode(111);
+//        debitCard->SetBillingAddress("111 hello st, BC, Canada");
+//        customrUser->AddPaymentCard(*debitCard);
+//
+//        CreditCard* creditCard = new CreditCard();
+//        creditCard->SetOwnerName(customrUser->GetName());
+//        creditCard->SetCardNumber("22-11-03-44");
+//        creditCard->SetExpiryDate("11/25");
+//        creditCard->SetSecurityCode(1117);
+//        customrUser->AddPaymentCard(*creditCard);
+//        userame_userobject_map[customrUser->GetUserName()] = customrUser;
+
+    }
 
     void Login() {
         bool x = false;
@@ -105,10 +143,10 @@ public:
             cout << i << "\n";// ? Need Update [Itenerary.ToString]
         }
     }
-    void tempFillItenrary(const string &name){
-        for(int i=0;i<10;i++)
-            UserToItenrary[name].push_back("\t#"+name);
-    }
+//    void tempFillItenrary(const string &name){
+//        for(int i=0;i<10;i++)
+//            UserToItenrary[name].push_back("\t#"+name);
+//    }
 
     ~UserManager() {
         users.clear();
